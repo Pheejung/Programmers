190fd4f55cac5e69a7a60124a459718dbe0bc92a
heejung Park <gmlwjd2116@naver.com>
2023-12-08 02:20:25 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,9 +1,11 @@
 function solution(n) {
     var answer = 0;
-     for( let i  =0; i <=n; i++ ) {
-        if( n % i === 0 && i % 2 === 1 )
-            answer ++;
-     }
-       
+    let  i = 0;
+    while(n > 0){
+        i++;
+       if(n % i  === 0)  answer++;
+        n -= i;
+    }
     return answer;
 }
+
