4f6b44b3781d192eb4c7d824f053ea2d0550995b
heejung Park <gmlwjd2116@naver.com>
2023-12-14 12:26:46 +0900

    [level 2] Title: 프로세스, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,24 +1,28 @@
 function solution(priorities, location) {
-  const processes = priorities.map((priority, index) => ({
-    priority,
-    isTarget: index === location,
-  }));
-
-  let order = 0;
+    var answer = 0;
+    
+     const processes = priorities.map((priority, index) => ({
+         priority,
+         isTarget: index === location
+     }));
+    
 
   while (processes.length > 0) {
-    const currentProcess = processes.shift();
-    const hasHigherPriority = processes.some((process) => process.priority > currentProcess.priority);
-
+      const currentProcess = processes.shift();
+      const hasHigherPriority = processes.some((process) => process.priority > currentProcess.priority);
+      
     if (hasHigherPriority) {
       processes.push(currentProcess);
     } else {
-      order += 1;
+      answer += 1;
       if (currentProcess.isTarget) {
-        return order;
+        return answer;
       }
     }
+      
   }
-
-  return order;
+    
+    
+    
+    return answer;
 }
